b6f1ae9a10917b9d0145b0b01f4912fcc4424991
Omkar Prabhu <omkkarprabuinfinity@gmail.com>
2017-06-07 17:53:07 +0530

    Initial input section

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+
+$(function() {
+
+
+$('.form-container').submit(function(){
+
+    setTimeout(
+        function() {
+
+        },
+        5000);
+    $('.main').remove();
+    $('.load').css('display','flex');
+
+    loadData()
+});
+
+
+
+
+});
+
+
+function loadData() {
+
+
+
+    // add content
+
+
+
+    return false;
+};
